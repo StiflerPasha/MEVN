5e14735834a68dc38c38b8af381e304987c77454
StiflerPasha <dvoynospavel@gmail.com>
2019-11-29 17:37:31 +0300

    make it better

--- client/src/components/PostItemComponent.vue
+++ client/src/components/PostItemComponent.vue
@@ -1,48 +1,48 @@
 <template>
-	<v-col cols="12"
-	       class="post pt-0"
-	       @dblclick="() => this.$emit('delete', post._id)">
-		<v-card color="#242424">
-			
-			<v-row>
-				<v-col cols="auto"
-				       class="delete d-flex align-center ml-2"
-				       @click="() => this.$emit('delete', post._id)">
-					<v-btn icon>
-						<v-icon>mdi-trash-can-outline</v-icon>
-					</v-btn>
-				</v-col>
-				
-				<v-col class="pl-0">
-					<v-card-text class="text-center amber--text text--darken-3">
-						{{post.text}}
-					</v-card-text>
-					<v-divider dark></v-divider>
-					<p class="mb-0 pr-4 caption blue--text text--lighten-1 text-right">
-						{{ `${post.createdAt.toLocaleDateString()}` }}
-					</p>
-				</v-col>
-			</v-row>
-		
-		</v-card>
-	</v-col>
+  <v-col cols="12" class="post pt-0"
+         @dblclick="() => this.$emit('delete', post._id)">
+    <v-card color="#242424">
+
+      <v-row>
+        <v-col cols="auto" class="delete d-flex align-center ml-2"
+               @click="() => this.$emit('delete', post._id)">
+          <v-btn icon>
+            <v-icon>mdi-trash-can-outline</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col class="pl-0">
+          <v-card-text class="text-center amber--text text--darken-3">
+            {{post.text}}
+          </v-card-text>
+
+          <v-divider dark/>
+
+          <p class="mb-0 pr-4 caption blue--text text--lighten-1 text-right">
+            {{ `${post.createdAt.toLocaleDateString()}` }}
+          </p>
+        </v-col>
+      </v-row>
+
+    </v-card>
+  </v-col>
 </template>
 
 <script>
-	export default {
-		name: 'PostItemComponent',
-		props: ['post'],
-	};
+  export default {
+    name: 'PostItemComponent',
+    props: ['post']
+  };
 </script>
 
 <style scoped>
-	.delete {
-		background-color: darkred;
-		border-top-left-radius: 8px;
-		border-bottom-left-radius: 8px;
-	}
-	
-	.delete:hover {
-		cursor: pointer;
-	}
+  .delete {
+    background-color: darkred;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+  }
+
+  .delete:hover {
+    cursor: pointer;
+  }
 </style>
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -6,37 +6,40 @@ const uri = process.env.DB_CONNECT;
 
 const router = express.Router();
 
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  
+  return client.db('mevn').collection('posts');
+}
+
 // Get Posts
 router.get('/', async (req, res) => {
-	const posts = await loadPostsCollection();
-	res.send(await posts.find({}).toArray());
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Posts
 router.post('/', async (req, res) => {
-	const posts = await loadPostsCollection();
-	await posts.insertOne({
-		text: req.body.text,
-		createdAt: new Date(),
-	}, () => console.log('Add Post'));
-	res.status(201).send();
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  }, () => console.log('Add Post'));
+  res.status(201).send();
 });
 
 // Delete Posts
 router.delete('/:id', async (req, res) => {
-	const posts = await loadPostsCollection();
-	await posts.deleteOne({
-		_id: new mongodb.ObjectID(req.params.id),
-	}, () => console.log('Delete Post'));
-	res.status(200).send();
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({
+    _id: new mongodb.ObjectID(req.params.id)
+  }, () => console.log('Delete Post'));
+  res.status(200).send();
 });
-
-async function loadPostsCollection() {
-	const client = await mongodb.MongoClient.connect
-	(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	
-	return client.db('mevn').collection('posts');
-}
 
 
 module.exports = router;
